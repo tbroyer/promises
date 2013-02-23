8dfc0b5eeb2d5dd527a66cf0dbb2993e45fb1c42
Thomas Broyer <t.broyer@ltgt.net>
2013-02-23 16:13:23 +0100

    Add JAX-RS 2.0 support.

---
+++ src/main/java/net/ltgt/promises/rs/client/InvocationCallbackPromise.java
@@ -0,0 +1,42 @@
+package net.ltgt.promises.rs.client;
+
+import javax.ws.rs.client.InvocationCallback;
+import javax.ws.rs.core.GenericType;
+
+import net.ltgt.promises.ForwardingPromise;
+import net.ltgt.promises.FulfillablePromise;
+import net.ltgt.promises.Promise;
+
+/**
+ * An {@link InvocationCallback} that is also a {@link Promise}.
+ * <p>
+ * Because of the way JAX-RS works, you have to create a subclass (generally anonymous) of
+ * {@link InvocationCallbackPromise} in order to <i>record</i> the generic type argument into
+ * the type, so that JAX-RS can <i>read</i> using reflection; similar to {@link GenericType}.
+ * <p>
+ * For example:
+ * <pre><code>
+ * InvocationCallbackPromise&lt;JsonObject> promise = new InvocationCallbackPromise&ltJsonObject>() {};
+ * asyncInvoker.get(promise);
+ * promise.then(…).then(…);
+ * </code></pre>
+ */
+public abstract class InvocationCallbackPromise<V> extends ForwardingPromise<V> implements InvocationCallback<V> {
+
+  private final FulfillablePromise<V> promise = FulfillablePromise.create();
+
+  @Override
+  public void completed(V response) {
+    promise.fulfill(response);
+  }
+
+  @Override
+  public void failed(Throwable throwable) {
+    promise.reject(throwable);
+  }
+
+  @Override
+  protected Promise<V> getDelegate() {
+    return promise;
+  }
+}
---
+++ src/main/java/net/ltgt/promises/rs/container/AsyncResponseCallback.java
@@ -0,0 +1,36 @@
+package net.ltgt.promises.rs.container;
+
+import java.util.Objects;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.AsyncResponse;
+
+import net.ltgt.promises.Promise.LeafCallback;
+
+public final class AsyncResponseCallback extends LeafCallback<Object> {
+
+  public static AsyncResponseCallback create(AsyncResponse response) {
+    return new AsyncResponseCallback(response);
+  }
+
+  private final AsyncResponse response;
+
+  private AsyncResponseCallback(AsyncResponse response) {
+    this.response = Objects.requireNonNull(response);
+  }
+
+  @Override
+  public void onFulfilled(Object value) {
+    response.resume(value);
+  }
+
+  @Override
+  public void onRejected(Throwable reason) {
+    // FIXME: workarounds http://java.net/jira/browse/JERSEY-1753
+    if (reason instanceof WebApplicationException) {
+      response.resume(((WebApplicationException) reason).getResponse());
+    } else if (reason instanceof RuntimeException) {
+      response.resume(reason);
+    }
+  }
+}
